cab1df6748b3eb6b3a4d313e23a3e3cff9c461a6
username-temporary <vagiannisefstratioscoms@gmail.com>
2026-02-15 02:28:04 +0200

    generation: 24

--- home.nix
+++ home.nix
@@ -35,7 +35,13 @@
     #   echo "Hello, ${config.home.username}!"
     # '')
   ];
-
+  #bashrc configuration
+  programs.bash={
+    enable=true;
+    initExtra=''
+    export PS1='\[\e[96;1m\]|\[\e]0;\u@\h: \w\a\]\u@:\[\e[22m\]\W\[\e[1m\]|\$\a\[\e[0m\]' \n
+    '';
+  };
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
