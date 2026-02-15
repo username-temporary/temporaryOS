3b8ed46358ecfefff181e7ddd2949c94aaf90167
username-temporary <vagiannisefstratioscoms@gmail.com>
2026-02-15 15:08:42 +0200

    generation: 29

--- home.nix
+++ home.nix
@@ -45,6 +45,7 @@
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
+    "~/.config/hyprland.conf".source=./home/hyprland.conf;
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
