88466369379a6a379c18776b7d6b157776f84e85
username-temporary <vagiannisefstratioscoms@gmail.com>
2026-02-18 18:34:59 +0200

    generation: 82

--- configuration.nix
+++ configuration.nix
@@ -130,16 +130,5 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.11"; # Did you read the comment?
 ### sad attempt at fixing the wifi
-  systemd.services.wpa_supplicant.enable=true;
-  systemd.services.wpa_supplicant.environment.OPENSSL_CONF = pkgs.writeText"openssl.cnf"''
-openssl_conf = openssl_init
-[openssl_init]
-ssl_conf = ssl_sect
-[ssl_sect]
-system_default = system_default_sect
-[system_default_sect]
-Options = UnsafeLegacyRenegotiation
-[system_default_sect]
-CipherString = Default:@SECLEVEL=0
-'';
+
 }
