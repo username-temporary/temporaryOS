cd59ca7e4fe86632e618c3a3f19b04177b8ffd89
username-temporary <vagiannisefstratioscoms@gmail.com>
2026-02-18 21:36:16 +0200

    generation: 85

--- configuration.nix
+++ configuration.nix
@@ -28,15 +28,22 @@
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
   networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+#  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
+  
   networking.networkmanager.enable = true;
-  services.gnome.gnome-keyring.enable=true;
+# networking.networkmanager.unmanaged = [
+#   "*"
+#   "except:type:wwan"
+#   "except:type:gsm"
+# ];
+  
+services.gnome.gnome-keyring.enable=true;
 
   # Set your time zone.
   time.timeZone = "Europe/Athens";
