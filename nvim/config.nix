4e96a21acc382009ce51a7587e02ba502635309c
username-temporary <vagiannisefstratioscoms@gmail.com>
2026-02-19 23:29:42 +0200

    generation: 98

--- nvim/config.nix
+++ nvim/config.nix
@@ -13,11 +13,11 @@
 #       
 #     };
       options = {
-#        tabstop = 2;
-#       shiftwidth = 2;
-#       softtabstop = 2;
-#       expandtab = true;
-#
+        tabstop = 2;
+        shiftwidth = 2;
+        softtabstop = 2;
+        expandtab = true;
+
         autoindent = true;
         smartindent = true;
      };
