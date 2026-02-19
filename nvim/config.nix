7bf7b4bc670a299d329494ea1636b783d2aa8e34
username-temporary <vagiannisefstratioscoms@gmail.com>
2026-02-19 09:01:11 +0200

    generation: 87

--- nvim/config.nix
+++ nvim/config.nix
@@ -1,18 +1,18 @@
-{pkgs,lib,...}
+{pkgs,lib,...}:
 {
   vim={
-    statusline.lualine.enable=true;
-    telescope.enable=true;
-    autocomplete.nvim-cpm.enable=true;
-
-    
-    languages={
-      enableLSP=true;
-      enableTreesitter=true;
-      nix.enable=true;
-      
-    };
-
-  };
+#   statusline.lualine.enable=true;
+#   telescope.enable=true;
+#   autocomplete.nvim-cpm.enable=true;
+#
+#   
+#   languages={
+#     enableLSP=true;
+#     enableTreesitter=true;
+#     nix.enable=true;
+#     
+#   };
+#
+ };
 
 }
