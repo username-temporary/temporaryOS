0befc14e93dbb75515941ca3f18994193b1970d4
username-temporary <vagiannisefstratioscoms@gmail.com>
2026-03-11 12:10:38 +0200

    generation: 25

--- packages/gaming.nix
+++ packages/gaming.nix
@@ -5,7 +5,6 @@
   programs.gamemode.enable = true;
 #I will add any packages I need for gaming here I just can't  think of any right now :/
   environment.systemPackages= with pkgs;[
-  atlauncher
 
   ];
 
